476a7cf4810cc2eeba2bc04787b5a3191208cbfb
Perry Angelora <pea.mail.list@gmail.com>
2026-03-04 20:34:33 -0500

    Minor version updates - Removed ESLint in favor of Biome

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -20,7 +20,11 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
           },
         },
       },
